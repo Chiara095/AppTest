aa3b39edd980c37095289463282de712c7f18830
Chiara095 <160756278+Chiara095@users.noreply.github.com>
2024-05-22 23:19:43 +0200

    Update Streamlit_Test_ErrLogging.py

--- Streamlit_Test_ErrLogging.py
+++ Streamlit_Test_ErrLogging.py
@@ -1,3 +1,4 @@
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 import streamlit as st
 import openai
 import torch
@@ -8,6 +9,8 @@ import os
 import requests
 from zipfile import ZipFile
 from io import BytesIO
+
+os.system(“curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y”)
 
 # Function to check and download the model files
 def download_and_unzip(url, extract_to='model_directory'):
