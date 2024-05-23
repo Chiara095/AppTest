5bb096d9ab60aabc3c45afb7f99eec63d5cc43ec
Chiara095 <160756278+Chiara095@users.noreply.github.com>
2024-05-23 12:28:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,13 @@
+import os
 
+# Install Rust
+os.system("curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y")
+
+# Add Rust to PATH
+os.system("source $HOME/.cargo/env")
+
+import streamlit as st
+import openai
+import torch
+from transformers import AutoTokenizer, CamembertForSequenceClassification
+from sentence_transformers import SentenceTransformer, util
