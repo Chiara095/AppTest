cd2e8d0d626525154b573b75f7d4b0e81ea69b63
Chiara095 <160756278+Chiara095@users.noreply.github.com>
2024-05-23 12:39:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,30 @@ import openai
 import torch
 from transformers import AutoTokenizer, CamembertForSequenceClassification
 from sentence_transformers import SentenceTransformer, util
+
+# Function to check and download the model files
+def download_and_unzip(url, extract_to='model_directory'):
+    try:
+        if not os.path.exists(extract_to):
+            os.makedirs(extract_to)
+            st.info('Downloading model files...')
+            r = requests.get(url)
+            if r.status_code == 200:
+                z = ZipFile(BytesIO(r.content))
+                z.extractall(path=extract_to)
+                st.success('Downloaded and extracted model files successfully!')
+            else:
+                st.error(f"Failed to download model files: Status code {r.status_code}")
+        else:
+            st.info('Model files already downloaded.')
+    except Exception as e:
+        st.error(f"Failed to download or extract model files: {e}")
+
+# URL of the zip file on Google Drive
+zip_file_url = 'https://drive.google.com/uc?export=download&id=1CfXmznt24jEHRyymtxYg7aiyyN6AdY-k'
+
+#Link to file for visualization: https://drive.google.com/file/d/1CfXmznt24jEHRyymtxYg7aiyyN6AdY-k/view?usp=sharing
+#ZIP FILE ID: 1CfXmznt24jEHRyymtxYg7aiyyN6AdY-k
+
+# Download and unzip model files if needed
+download_and_unzip(zip_file_url)
