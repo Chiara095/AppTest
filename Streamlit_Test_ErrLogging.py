daac103930119d5446c42a6c54ce096875dce2a3
Chiara095 <160756278+Chiara095@users.noreply.github.com>
2024-05-23 12:00:42 +0200

    Update Streamlit_Test_ErrLogging.py

--- Streamlit_Test_ErrLogging.py
+++ Streamlit_Test_ErrLogging.py
@@ -1,13 +1,1 @@
-#curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-import streamlit as st
-import openai
-import torch
-from transformers import AutoTokenizer, CamembertForSequenceClassification
-from sentence_transformers import SentenceTransformer, util
-import pycountry
-import os
-import requests
-from zipfile import ZipFile
-from io import BytesIO
 
-#os.system(“curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y”)
